6b775f147bae0262dcb131a6440d0659f09adcd3
Mozoou <m.zouheir74@gmail.com>
2023-04-08 22:40:24 +0200

    added fix (codacy)

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -4,23 +4,26 @@ namespace App\Controller;
 
 use Core\Database\Db;
 use Twig\Environment;
-use Core\Router\Router;
 use Cocur\Slugify\Slugify;
 use Berlioz\FlashBag\FlashBag;
-use Goenitz\Request\Request;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Twig\Loader\FilesystemLoader;
 use Twig\Extension\DebugExtension;
-use Ghostwriter\Environment\Environment as Env;
 
 abstract class Controller
 {
     protected ?Db $db = null;
+
     protected ?FlashBag $flash = null;
+
     protected ?Environment $twig = null;
-    protected ?Router $router = null;
+
     protected ?Slugify $slugify = null;
+
+    protected ?Session $session = null;
+
     protected ?Request $request = null;
-    protected ?Env $env = null;
 
     public function __construct()
     {
@@ -32,17 +35,16 @@ abstract class Controller
             'debug' => true
         ]);
         $this->twig->addExtension(new DebugExtension);
-        $this->router = Router::getInstance();
         $this->slugify = new Slugify();
-        $this->request = new Request();
-        $this->env = new Env();
+        $this->session = new Session();
+        $this->request = Request::createFromGlobals();
     }
 
-    public function render(string $path, ?array $params = []): string
+    public function render(string $path, ?array $params = []): void
     {
-        return $this->twig->render($path, [
+        echo $this->twig->render($path, [
             ...$params,
-            'user' => $this->env->hasServerVariable('user') ? $this->env->getServerVariable('user') : null,
+            'user' => $this->session->get('user', null),
             'flashes' => $this->flash->all(),
         ]);
     }
