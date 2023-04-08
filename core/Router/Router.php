965caff2fdaf1913691b4be77ab363bcfdc0e852
Mozoou <m.zouheir74@gmail.com>
2023-04-08 22:32:31 +0200

    fixed Router class (codacy)

--- core/Router/Router.php
+++ core/Router/Router.php
@@ -2,41 +2,68 @@
 
 namespace Core\Router;
 
-use Ghostwriter\Environment\Environment;
+use Symfony\Component\HttpFoundation\Request;
 
 class Router
 {
     private const METHOD_GET = 'GET';
     private const METHOD_POST = 'POST';
+
+    private ?Request $request = null;  
     public array $handlers = [];
 
     private static ?self $_instance = null;
 
-    public static function getInstance()
+    public function __construct()
     {
-        if (null === self::$_instance) {
+        $this->request = Request::createFromGlobals();
+    }
+
+
+    /**
+     * Retreive instance of Router (singelton)
+     * @return self
+     */
+    public static function getInstance(): self
+    {
+        if (self::$_instance === null) {
             self::$_instance = new Router();
         }
         return self::$_instance;
     }
 
+    /**
+     * Add a get route
+     * @param string $path
+     * @param string $controller
+     * @param string $controllerMethod
+     * @return void
+     */
     public function get(string $path, string $controller, string $controllerMethod): void
     {
         $this->addHandler($path, self::METHOD_GET, $controller, $controllerMethod);
     }
 
+    /**
+     * Add a post route
+     * @param string $path
+     * @param string $controller
+     * @param string $controllerMethod
+     * @return void
+     */
     public function post(string $path, string $controller, string $controllerMethod): void
     {
         $this->addHandler($path, self::METHOD_POST, $controller, $controllerMethod);
     }
 
-    public function run(): void
+    /**
+     * Run controllerMethod route called
+     * @return void
+     */
+    public function run(): callable | null
     {
-        //[x]TODO: Direct use of $_SERVER Superglobal detected.        
-        $environment = new Environment();
-        $requestUri = parse_url($environment->getServerVariables()['REQUEST_URI']);
-        $requestMethod = $environment->getServerVariables()['REQUEST_METHOD'];
-        $requestPath = $requestUri['path'];
+        $requestPath = $this->request->getPathInfo();
+        $requestMethod = $this->request->getMethod();
         $callback = null;
 
         foreach ($this->handlers as $handler) {
@@ -50,12 +77,20 @@ class Router
         }
 
         if (!$callback) {
-            return;
+            return null;
         }
 
-        call_user_func_array($callback, []);
+        return $callback();
     }
 
+    /**
+     * Add a route to handlers array
+     * @param string $path
+     * @param string $method
+     * @param string $controller
+     * @param string $controllerMethod
+     * @return void
+     */
     private function addHandler(string $path, string $method, string $controller, string $controllerMethod): void
     {
         $this->handlers[$method . $path] = [
